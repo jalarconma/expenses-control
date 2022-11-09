88ac1514b1efd35b3a3975a09e6f916b602cfd21
Julian Alarcon <juaalarconma@gmail.com>
2022-11-09 18:40:07 -0500

    add hello world api

--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ import app from './app.mjs';
 import './configs/loadEnv.mjs'
 
 const server = http.createServer(app);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8081;
+
+app.get('/hello', (req, res) => {
+  res.json({message: 'Hello World'})
+})
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
